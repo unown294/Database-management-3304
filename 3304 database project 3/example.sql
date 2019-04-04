454e0ccf3825dddda83983939dd0d1448bd9d67a
Dalton <trainguy294@gmail.com>
2019-04-04 08:02:28 -0500

    Add files via upload
    Updates for this file as of 4/3/2019

--- 3304 database project 3/example.sql
+++ 3304 database project 3/example.sql
@@ -35,13 +35,12 @@ GROUP BY CustType;
 
 --Problem 10 Example. List the ResNum, CheckIn, CheckOut, and the total nightly rate charged for Reservation 1001. Use the following headings:
 -- ResNumber, CkInDate, CkOutDate, NightlyTtl
---Close the output file
 SELECT R.ResNum as 'ResNumber', CheckIN as 'CkInDate', CheckOut as 'CkOutDate', TO_CHAR(SUM(RateAmt), '$999,999.99') as 'NightlyTtl'
   FROM Reservation R, ResDetail RD
  WHERE R.ResNum = RD.ResNum AND 
 	   R.ResNum = 1001;
 
--- List the CustID, CustFName, CustLName, and count of reservation for each customer. Cpmbine the first and last name into one column
+-- List the CustID, CustFName, CustLName, and count of reservation for each customer. Combine the first and last name into one column
 --Sort by reservation count in descending order, then by customer ID in ascending order. Hint:  Use a GROUP BY clause.
 SELECT R.CustID, CustFName || ' ' || CustLName, Count(ResID) as 'ResCount' -- || is used for string cancatination which joins the values together into one column without a space
 FROM Reservation_djr R, Customer_djr C
@@ -64,24 +63,36 @@ GROUP BY A.Agentype, AgentDesc;
 
 --Other join types
 
---Problem 13 example. List the ResID, CustID, CustLName, CustPhone for all reservations. Format the phone number as ###.###.####. Use the following
---alternate column headings: ResID CustomerID, LastName, Phone. Sort by ResID
-SELECT ResID as 'ResID', C.CustID as 'CustomerID', CustLName as 'LastName', SUBSTR(CustPhone, 1,3) || '.' || SUBSTR(CustPhone, 4,3) || '.' || SUBSTR(CustID, 7,4) as 'Phone'
+--Problem 13 example. List the ResNum, CustID, CustLName, CustPhone for all reservations. Format the phone number as ###.###.####. Use the following
+--alternate column headings: ResNum CustomerID, LastName, Phone. Sort by ResNum
+SELECT ResNum as 'ResID', C.CustID as 'CustomerID', CustLName as 'LastName', SUBSTR(CustPhone, 1,3) || '.' || SUBSTR(CustPhone, 4,3) || '.' || SUBSTR(CustID, 7,4) as 'Phone'
 FROM Reservation_djr R, Customer_djr C
 WHERE R.CustID = C.CustID
-ORDER BY ResID;
+ORDER BY ResNum;
 
 --Problem 14 example. 
-
-SELECT ResNum, RD.RoomNum, R.RoomType, RoomDesc, RateAmt
+SELECT ResNum, RD.RoomNum, R.RoomType, RoomDesc, RateAmt		--Whats being outputed
 FROM ResDetail_djr RD, Room_djr R, RoomType_djr RT
 WHERE RD.RoomNum = R.RoomNum AND
 	  R.RoomType = RT.RoomType AND
-ResID, RateAmt IN
-	(SELECT ResID, MAX(RateAmt)
-	 FROM ResDetail_djr
-	 GROUP BY ResID);
+	  (ResNum, RateAmt) IN				--"JOIN" IN is necessary to show only a specific amount rather than a bunch of columns
+	  (SELECT ResNum, MAX(RateAmt)		--This is the nested select that only displays the combinations that have the highest rent amount
+	  FROM ResDetail_djr
+	  GROUP BY ResNum);
 
+--Problem 24 example	  
+SELECT RD.RoomNum, R.RoomType, RoomDesc, RateAmt
+FROM ResDetail_djr RD, Room_djr R, RoomType_djr RT
+WHERE RD.RoomNum = R.RoomNum AND
+	  R.RoomType = RT.RoomType AND
+	  ResNum = 1008 AND
+	  RateAmt = 
+	  (SELECT MIN(RateAmt)
+	     FROM ResDetail_djr
+	    WHERE ResNum = 1008);
+
+
+--Close the output file	  
 --Turns off Spooling and echoing
 spool off
 set echo off
