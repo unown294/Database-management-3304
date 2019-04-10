66b666efdfbe4119bb4885cb866c99d61171856c
Dalton <trainguy294@gmail.com>
2019-04-10 09:58:15 -0500

    Add files via upload

--- Project 2/Tables.sql
+++ Project 2/Tables.sql
@@ -7,35 +7,35 @@ set echo on
 
 CREATE TABLE Rating_djr(
 	RatingID	char(6)		NOT NULL	PRIMARY KEY,
-	RatingDesc	varchar(36)
+	RatingDesc	varchar(36)	NOT NULL
 );
 
 CREATE TABLE Genre_djr(
 	GenreID		char(2)		NOT NULL	PRIMARY KEY,
-	GenreDesc	varchar(36)
+	GenreDesc	varchar(36)	NOT NULL
 );
 
 CREATE TABLE Customer_djr(
 	CustID		number(4) 	NOT NULL	PRIMARY KEY,
-	CustFName	char(16),
-	CustLName	char(16),
+	CustFName	char(16)	NOT NULL,
+	CustLName	char(16)	NOT NULL,
 	CustPhone	char(10)
 );
 
 CREATE TABLE Clerk_djr(
 	ClerkID		number(4) 	NOT NULL	PRIMARY KEY,
-	ClerkFName	char(16),
-	ClerkLName	char(16)
+	ClerkFName	char(16)	NOT NULL,
+	ClerkLName	char(16)	NOT NULL
 );
 
 CREATE TABLE Fee_djr(
 	FeeCode		char(1) 	NOT NULL 	PRIMARY KEY,
-	FeeAmt		number(3,2)
+	FeeAmt		number(3,2)	NOT NULL
 );
 
 CREATE TABLE Format_djr(
 	FormatCode	char(1)     NOT NULL	PRIMARY KEY,
-	FormatDesc	char(20)
+	FormatDesc	char(20)	NOT NULL
 );
 
 CREATE TABLE Rental_djr(
@@ -49,7 +49,7 @@ CREATE TABLE Rental_djr(
 
 CREATE TABLE Title_djr(
 	TitleID		number(4)	NOT NULL	 PRIMARY KEY,
-	Title		varchar(36),
+	Title		varchar(36)	NOT NULL,
 	RatingID	char(6)		NOT NULL,
 	GenreID		char(2)		NOT NULL,
 	CONSTRAINT con_Rating FOREIGN KEY (RatingID) REFERENCES Rating_djr(RatingID),
@@ -74,6 +74,17 @@ CREATE TABLE RentalDetail_djr(
 	CONSTRAINT con_Rental FOREIGN KEY (RentailID) REFERENCES Rental_djr(RentailID)
 );
 
+DESCRIBE RentalDetail_djr;
+DESCRIBE Title_djr;
+DESCRIBE Disk_djr;
+DESCRIBE Rental_djr;
+DESCRIBE Format_djr;
+DESCRIBE Fee_djr;
+DESCRIBE Genre_djr;
+DESCRIBE Customer_djr;
+DESCRIBE Clerk_djr;
+DESCRIBE Rating_djr;
+
 --Turns off Spooling and echoing
-spool Project2_djr.txt
+spool off
 set echo off
